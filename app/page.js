308edded999bb06077b3fbd02b4135aa40a99c42
Muhammad Danish <125806096+Danish-Razzaq@users.noreply.github.com>
2024-03-01 16:11:10 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
   try {
-    const res = await fetch("https://localhost:3000/api/Tickets", {
+    const res = await fetch("https://ticket-app-steel.vercel.app/api/Tickets", {
       cache: "no-store",
     });
     return res.json();
